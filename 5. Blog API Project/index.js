6371a0b2549d64664aaf6b2293584caa1802434f
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2024-07-21 20:28:26 +0530

    fix a bug in index.js(Blog API Project)

--- 5. Blog API Project/index.js
+++ 5. Blog API Project/index.js
@@ -93,6 +93,4 @@ app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`API is running at http://localhost:${port}`);
-});
+
